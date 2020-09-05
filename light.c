5adaad54de9b2c8a859856af8a6b4c6e53621031
root <root@host-79-165-96-32.qwerty.ru>
2020-09-04 21:56:01 -0400

    comments

--- light.c
+++ light.c
@@ -9,15 +9,19 @@ t_color     reflection_color(t_point *P, t_point *N, t_color *s_color)
     t_color result_color;
 
     i = 0;
-    //эта часть не работает, потому что хз, но цвет меняется нормально
+    //тут пока только заполняющий источник света
     light.intencity = 0.2;
     i += light.intencity;
     light.position = get_point(WID/2 + 100, HEI/2, 20);
     light.direction = get_point(2, 1, 0);
+    //берем вектор из точки пересечения P к позиции источника света
     L = vector_sub(&light.position, P);
+    //скалярное произвеление нормали и позиции света
     n_dot_l = vector_dot(N, &light.position);
+    //складываем все это дело для получаение коэффициента i
+    //i может быть больше единицы
     if (n_dot_l > 0)
-        i += light.intencity * n_dot_l / (vector_length(&L));
+        i += light.intencity * n_dot_l /  (vector_length(N) * vector_length(&L));
     result_color.red = s_color->red * i;
     result_color.green = s_color->green * i;
     result_color.blue = s_color->blue * i;
--- sphere.c
+++ sphere.c
@@ -50,10 +50,18 @@ int intersect_ray_sphere(t_ray *r, t_sphere *s, t_color *reflected_color)
     t_point intersection_point;
     t_point normal;
 
+    /*поиск точек пересечения луча и сферы по квадратному уравнению, части которого:
+    a - скалярное происзведение направления луча,
+    dist - координаты вектора от центра сферы к центру луча
+    b - скалярное произведение вектора направления луча и вектора из центра сферы
+    c - разница скалярного произведения вектора из центра сферы к началу луча и квадрата радиуса сферы*/
     a = vector_dot(&r->dir, &r->dir);
     dist = vector_sub(&r->start, &s->center);
     b = 2 * vector_dot(&r->dir, &dist);
     c = vector_dot(&dist, &dist) - (s->radius * s->radius);
+    /*поиск решений квадратного уравнения
+    если решений нет - луч не пересекает сферу, если решение одно - проходит по касательной к сфере
+    если два решения - пересекает сферу в двух точках*/
     discr = b * b - 4 * a * c;
     if (discr < 0)
         return (0);
@@ -62,9 +70,14 @@ int intersect_ray_sphere(t_ray *r, t_sphere *s, t_color *reflected_color)
         sqrt_discr = sqrt(discr);
         t0 = (-b + sqrt_discr) / (2 * a);
         t1 = (-b - sqrt_discr) / (2 * a);
+        /*t0 и t1 - длина вектора до точек пересечения лучом сферы*/
         t_point buf;
         if (t0 > t1)
         {
+            /*выбираем ближайшую точку пересечения со сферой
+            умножаем направление луча на т1 или т0
+            для получения координат точки пересечения
+            прибавляем к координатам начала луча координаты вектора направления*/
             buf = vector_scale(t1, &r->dir);
             intersection_point = vector_add(&r->start, &buf);
         }
@@ -73,8 +86,16 @@ int intersect_ray_sphere(t_ray *r, t_sphere *s, t_color *reflected_color)
             buf = vector_scale(t0, &r->dir);
             intersection_point = vector_add(&r->start, &buf);
         }
+        /*для получения значения цвета в точке нужно послать луч, идущий по нормали от
+        точки пересечения изначального луча и сферы, в сторону источника света.
+        нормаль сферы в конкретной точке - единичный вектор, лежащий на прямой, проходящей
+        через центр сферы и искомую точку( в нашем случае через точку пересечения изначального луча и сферы)*/
+
         normal = vector_sub(&intersection_point, &s->center);
+        /*для получения вектора с единичной длиной нужно поделить его на квадратный корень из суммы
+        квадратов его компонентов(т.е из его скалярного произведения на самого себя)*/
         normal = vector_div_by_scalar(&normal, vector_length(&normal));
+        /*тут рассчитываем сам конечный цвет в файле light.c*/
         *(t_color *)reflected_color = reflection_color(&intersection_point, &normal, &s->color);
         return(1);
     }
